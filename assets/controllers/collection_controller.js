20e1f4ac4357acfc46c8be5a9bd53df34c8bbd3a
Arnaud BODEL <contact@osw3.net>
2022-06-03 17:11:05 +0200

    -

--- assets/controllers/collection_controller.js
+++ assets/controllers/collection_controller.js
@@ -2,15 +2,22 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
 
+    container;
     template;
 
-    connect() {
+    connect() 
+    {
+        this.container = this.element.querySelector('[data-collection-id]');
     }
 
     getTemplate() 
     {
         this.template = this.element.querySelector('template');
 
-        console.log(this.template);
+        if (this.template)
+        {
+            var clone = document.importNode(this.template.content, true);
+            this.container.appendChild( clone );
+        }
     }
 }
