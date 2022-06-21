7f93bb59d492e296f12c5f11384f2103d1a2d14b
Arnaud BODEL <contact@osw3.net>
2022-06-21 14:45:26 +0200

    -

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,15 @@
+<?php 
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: "app_profile")]
+    public function index(): Response
+    {
+        return $this->render('profile/index.html.twig');
+    }
+}
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -37,8 +37,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            // TODO: redirect to login page
-            return $this->redirectToRoute('app_homepage');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
