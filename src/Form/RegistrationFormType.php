08739b0ea217d86987ac7f36b9bf069e07c33b63
Arnaud BODEL <contact@osw3.net>
2022-06-21 10:00:45 +0200

    -

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,16 +20,30 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+
+            // Firstname
+            ->add('firstname', TextType::class, [
+                'label' => "Prénom",
             ])
+
+            // Lastname
+            ->add('lastname', TextType::class, [
+                'label' => "NOM",
+            ])
+
+            // Birthday
+            ->add('birthday', BirthdayType::class, [
+                'label' => "Date de naissance"
+            ])
+
+            // Email
+            ->add('email', EmailType::class, [
+                'label' => "Adresse email"
+            ])
+
+            // Plain Password
             ->add('plainPassword', PasswordType::class, [
+                'label' => "Mot de passe",
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
@@ -40,6 +57,17 @@ class RegistrationFormType extends AbstractType
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
+                    ]),
+                ],
+            ])
+
+            // Agree Terms
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => "J'ai lu et j'accepte les conditions d'utilisation du site.",
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
                     ]),
                 ],
             ])
