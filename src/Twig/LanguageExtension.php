f4f2fc1ad4422258d0cce317419cbc50e8f7b2b3
Arnaud BODEL <contact@osw3.net>
2022-06-22 11:26:36 +0200

    -

--- src/Twig/LanguageExtension.php
+++ src/Twig/LanguageExtension.php
@@ -3,9 +3,19 @@ namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
+use Twig\Environment;
+
 
 class LanguageExtension extends AbstractExtension
 {
+    private $environment;
+
+    public function __construct(Environment $environment)
+    {
+        $this->environment = $environment;
+    }
+
+
     public function getFunctions(): array
     {
         return [
@@ -15,6 +25,6 @@ class LanguageExtension extends AbstractExtension
 
     public function doSelectLanguage(): string
     {
-        return "Select Lang !!!";
+        return $this->environment->render('components/language/select.html.twig');
     }
 }
