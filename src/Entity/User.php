0efdd206bb143d42dc3dc28dfe900ef7f0a6b9c1
Arnaud BODEL <contact@osw3.net>
2022-06-22 15:10:53 +0200

    -

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -112,8 +112,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
-        $roles[] = 'ROLE_TRUC';
-        $roles[] = 'ROLE_MACHIN';
 
         return array_unique($roles);
     }
