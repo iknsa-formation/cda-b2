a483d75627d2688c7c872c0b85a0fe6ccedcdb56
Arnaud BODEL <contact@osw3.net>
2022-06-22 12:11:20 +0200

    -

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -9,6 +9,12 @@ class RedirectController extends AbstractController
 {
     public function redirectToLocale(): Response
     {
-        return $this->redirectToRoute('app_homepage');
+        // Recupération de la locale par defaut definit dans le fichier "service.yaml
+        $locale = $this->getParameter('default_locale');
+
+        // Redirection de l'utilisateur vers la route "Homepage" avec le partamètre de langue part defaut
+        return $this->redirectToRoute('app_homepage', [
+            '_locale' => $locale
+        ]);
     }
 }
