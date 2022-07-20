fb82fe5eaff0a9afc02b73cb4ae558c643d94e96
Khalid Sookia <khalidsookia@gmail.com>
2022-07-20 11:50:13 +0200

    fixed prototype in js

--- public/new-book.js
+++ public/new-book.js
@@ -1,5 +1,8 @@
-if (document.getElementById('new_book_authors')) {
+const updateAuthors = function () {
+    /** @todo delete all childre from element */
     const element = document.getElementById('new_book_authors');
+    element.innerHTML = '';
+
     const prototype = element.dataset.prototype;
     const addNewButtonTemplate = `<button class="btn btn-primary" id="new-book-button">Ajouter un auteur</button>`;
     let counter = 0;
@@ -13,7 +16,7 @@ if (document.getElementById('new_book_authors')) {
             .replaceAll('__name__', counter);
 
         const deleteFieldButton = `<span class="btn btn-danger delete-field" data-field-id="${counter}">
-                                Remove author ${counter}</span>`;
+                            Remove author ${counter}</span>`;
 
         newField += deleteFieldButton;
         element.insertAdjacentHTML('beforeend', newField);
@@ -40,7 +43,7 @@ if (document.getElementById('new_book_authors')) {
     }
 }
 
-
+updateAuthors();
 
 const languageField = document.getElementById('new_book_language');
 
@@ -53,5 +56,14 @@ languageField.addEventListener('change', (event) => {
     fetch(window.location.href, { method: "POST", body: data })
         .then(function (response) {
             response.text()
+                .then(html => {
+                    const parser = new DOMParser();
+                    const page = parser.parseFromString(html, "text/html");
+
+                    const element = page.getElementById('new_book_authors');
+                    document.getElementById('new_book_authors').dataset.prototype = element.dataset.prototype;
+
+                    updateAuthors();
+                })
         });
 });
